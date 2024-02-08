f36be140103e96ca84a7fe242fb4b4e4edc7f6ec
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-02-09 00:52:36 +0530

    add : basic code for backend

---
+++ README.md
@@ -0,0 +1,5 @@
+## Hello in todo application
+It is a simple application and has the following features-
+- Anyone can create todo.
+- Anyone can see their existing todos.
+- Anyone can mark a todo as done.
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+
+const app=express();
+
+app.use(express.json());
+
+
+
+app.get('/todos',(req,res)=>{
+    
+})
+
+app.post('/todo',(req,res)=>{
+
+})
+
+app.put('/completed',(req,res)=>{
+
+})
